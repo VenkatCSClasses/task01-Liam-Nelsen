ae92922209a7f5e3028eba8dedab4fff9b58242a
Liam-Nelsen <lnelsen@ithaca.edu>
2026-01-30 18:02:55 -0500

    Added more equivalence cases and test passes

--- src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
@@ -28,6 +28,7 @@ public class BankAccount {
 
     /**
      * @post reduces the balance by amount if amount is non-negative and smaller than balance
+     * @throws InsufficientFundsException if amount is greater than balance
      */
     public void withdraw (double amount) throws InsufficientFundsException{
         if (amount <= balance){
--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -17,7 +17,7 @@ class BankAccountTest {
     }
 
     @Test
-    void withdrawTest() throws InsufficientFundsException{
+    void withdrawTest() throws InsufficientFundsException{  
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
         bankAccount.withdraw(100);
 
@@ -27,23 +27,73 @@ class BankAccountTest {
 
     @Test
     void isEmailValidTest(){
-        assertTrue(BankAccount.isEmailValid("a@b.com"));   // valid email address
+
+        //all valid emails
+        assertTrue(BankAccount.isEmailValid("a@b.com"));
         assertTrue(BankAccount.isEmailValid("glintwave92@orbimail.net"));
         assertTrue(BankAccount.isEmailValid("saffron.loop@tessera.io"));
         assertTrue(BankAccount.isEmailValid("emberlane@postcircuit.app"));
-        assertFalse(BankAccount.isEmailValid(""));          // empty string
+
+        //Equivalence class: tld
         assertFalse(BankAccount.isEmailValid("a@b"));       // equivalence class: bad domain - no tld
-        assertFalse(BankAccount.isEmailValid("a.b.com"));   // equivalence class: bad structure - no @
-        assertFalse(BankAccount.isEmailValid("@b.com"));    // equivalence class: bad structure - missing local part / border case
-        assertFalse(BankAccount.isEmailValid("a@.com"));    // equivalence class: bad structure - missing domain / border case
         assertFalse(BankAccount.isEmailValid("a@b."));      // equivalence class: bad domain - zero char tld / border case
+
+        //Equivalence class: domain
+        assertFalse(BankAccount.isEmailValid("a@.com"));    // equivalence class: bad structure - missing domain / border case
+
+        //Equivalence class: local
+        assertTrue(BankAccount.isEmailValid("a@b.com"));    // equivalence class: bad structure - missing local part / border case
+
+        //Equivalence class: character issue
+        assertFalse(BankAccount.isEmailValid(""));          // empty string
+        assertFalse(BankAccount.isEmailValid("a.b.com"));   // equivalence class: bad structure - no @
+        assertFalse(BankAccount.isEmailValid("a@b..com"));  // equivalence class: bad domain - double dot
+
+        //Equivalence class: Structure
         assertFalse(BankAccount.isEmailValid("a"));         // equivalence class: bad structure - single char / border case
         assertFalse(BankAccount.isEmailValid("@"));         // equivalence class: bad structure - zero char local and domain / border case
-        assertFalse(BankAccount.isEmailValid("a@b..com"));  // equivalence class: bad domain - double dot
-        assertFalse(BankAccount.isEmailValid("a..b@c.com"));// equivalence class: bad local part - double dot
 
-        // missing equivalence classes: checks for special/invalid characters, numbers, extra @ symbols
-        // missing border cases: either part ending/starting with dot, max-lengths
+        // Equivalence class: local part length
+        assertTrue(BankAccount.isEmailValid("ab@b.com")); // above boundary
+        assertTrue(BankAccount.isEmailValid("username@b.com")); // middle of boudary
+
+
+        // Equivalence class: domain must contain dot
+        assertFalse(BankAccount.isEmailValid("@ab.c")); // below boundary
+        assertTrue(BankAccount.isEmailValid("a@bc.de")); // above boundary
+        assertTrue(BankAccount.isEmailValid("a@example.com")); // middle of boundary
+
+
+        // Equivalence class: dot placement in local part
+        assertFalse(BankAccount.isEmailValid(".a@b.com")); //below boundary (leading dot)
+        assertTrue(BankAccount.isEmailValid("a.b@b.com")); //at boundary
+        assertTrue(BankAccount.isEmailValid("ab.cd@b.com")); //above boundary
+        assertTrue(BankAccount.isEmailValid("first.last@example.com")); //middle of boundary
+
+
+        // Equivalence class: dot placement in domain
+        assertFalse(BankAccount.isEmailValid("a@.com")); //below boundary (domain starts with dot)
+        assertTrue(BankAccount.isEmailValid("a@b.cd")); //above boundary
+        assertTrue(BankAccount.isEmailValid("a@example.com")); //middle of boundary
+
+
+        // Equivalence class: hyphen placement in domain
+        assertFalse(BankAccount.isEmailValid("a@-example.com")); //below boundary (domain starts with hyphen)
+        assertTrue(BankAccount.isEmailValid("a@ex-ample.com")); //at boundary
+        assertTrue(BankAccount.isEmailValid("a@exa-mple.com")); //above boundary
+        assertTrue(BankAccount.isEmailValid("a@example.com")); //middle of boundary
+
+
+        // Equivalence class: number of @ symbols
+        assertFalse(BankAccount.isEmailValid("a@@b.com")); //above boundary
+        assertTrue(BankAccount.isEmailValid("first.last@example.com")); //middle of boundary
+
+
+        // Equivalence class: illegal characters – below boundary (space in local part)
+        assertFalse(BankAccount.isEmailValid("a b@c.com")); //below boundary
+        assertTrue(BankAccount.isEmailValid("a_b@c.com")); //at boundary
+        assertTrue(BankAccount.isEmailValid("a-b@c.com")); //above boundary
+        assertTrue(BankAccount.isEmailValid("user.name@example.com")); //middle of boundary
     }
 
     @Test
